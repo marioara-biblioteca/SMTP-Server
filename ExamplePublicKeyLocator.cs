d236e3284cb73f870684206a1e4bf225375b7ef2
Cristiana Valeca <80671387+marioara-biblioteca@users.noreply.github.com>
2022-02-04 16:19:12 +0200

    Add files via upload

---
+++ ExamplePublicKeyLocator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Heijden.DNS;
+
+using Org.BouncyCastle.Crypto;
+
+using MimeKit;
+using MimeKit.Cryptography;
+
+namespace Common.SMTP 
+{ 
+    class ExamplePublicKeyLocator : DkimPublicKeyLocatorBase
+    {
+        readonly Dictionary<string, AsymmetricKeyParameter> cache;
+        readonly Resolver resolver;
+        public ExamplePublicKeyLocator()
+        {
+            cache = new Dictionary<string, AsymmetricKeyParameter>();
+
+            resolver = new Resolver("8.8.8.8")
+            {
+                TransportType = TransportType.Tcp,
+                UseCache = true,
+                Retries = 3
+            };
+        }
+
+        public AsymmetricKeyParameter DnsLookup(string domain, string selector)//, CancellationToken cancellationToken)
+        {
+            var query = selector + "._domainkey." + domain;
+            AsymmetricKeyParameter pubkey;
+
+            // checked if we've already fetched this key
+            if (cache.TryGetValue(query, out pubkey))
+                return pubkey;
+
+            // make a DNS query
+            var response = resolver.Query(query, QType.TXT);
+            var builder = new StringBuilder();
+
+            // combine the TXT records into 1 string buffer
+            foreach (var record in response.RecordsTXT)
+            {
+                foreach (var text in record.TXT)
+                    builder.Append(text);
+            }
+
+            var txt = builder.ToString();
+
+            // DkimPublicKeyLocatorBase provides us with this helpful method.
+            pubkey = GetPublicKey(txt);
+
+            cache.Add(query, pubkey);
+
+            return pubkey;
+        }
+
+        public override AsymmetricKeyParameter LocatePublicKey(string methods, string domain, string selector, CancellationToken cancellationToken = default(CancellationToken))
+        {
+        //    var methodList = methods.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+        //    for (int i = 0; i < methodList.Length; i++)
+        //    {
+        //        if (methodList[i] == "dns/txt")
+        //            return DnsLookup(domain, selector, cancellationToken);
+        //    }
+              throw new Exception("haha");
+        //    throw new NotSupportedException(string.Format("{0} does not include any suported lookup methods.", methods));
+        }
+
+        public override Task<AsymmetricKeyParameter> LocatePublicKeyAsync(string methods, string domain, string selector, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new Exception("haha");
+        //    return Task.Run(() => {
+        //        return LocatePublicKey(methods, domain, selector, cancellationToken);
+        //    }, cancellationToken);
+        }
+       
+    }
+}
---
+++ SMTP.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Common.SMTP {
+    class SMTP {
+
+        //gaseste toate emailurile dintr un text si retunreaza un array cu ele
+        public static string[] extractMails(string text) {
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*",
+            RegexOptions.IgnoreCase);
+            MatchCollection emailMatches = emailRegex.Matches(text);
+            List<string> result = new List<string>();
+            foreach (Match emailMatch in emailMatches)
+                result.Add(emailMatch.Value);
+            return result.ToArray();
+        }
+        public static bool checkRCPTIsInternal(string text) {
+            string[] mails = extractMails(text);
+            foreach (string mail in mails)
+                if (!isInternal(mail))
+                    return false;
+            return true;
+        }
+        public static bool isInternal(string email) {
+            if (email.EndsWith("@cristianavaleca.com"))
+                return true;
+            if (email.EndsWith("@localhost"))
+                return true;
+            if (email.EndsWith("@localhost.com"))
+                return true;
+            return false;
+        }
+    }
+    class SMTPMessage {
+        public string From;
+        public List<string> To = new List<string>();
+        public List<string> Data = new List<string>();
+        internal void SaveAndSend() {
+            Save();
+            string username;
+            int pos;
+            foreach (var to in this.To) 
+            {
+                var mail = SMTP.extractMails(to);
+                if (!SMTP.isInternal(mail[0]))
+                    SMTPServer.Sender.Send(mail[0], Data, From);
+                else
+                {
+                    username = SMTP.extractMails(To[0])[0];
+                    pos = username.IndexOf('@');
+                    username = username.Substring(0, pos);
+                    SaveInternal(username, true);
+                }
+            }
+            if(SMTP.isInternal(From))
+            {
+                username = SMTP.extractMails(From)[0];
+                pos = username.IndexOf('@');
+                username = username.Substring(0, pos);
+                SaveInternal(username, false);
+            }
+
+        }
+        
+        internal void Save() {
+            DirectoryInfo directory = SMTPServer.SMTPServer.ReceivedPath;
+            directory.Create();
+            string uniqueFileName = Path.Combine(directory.FullName, System.Guid.NewGuid().ToString() + ".smtp");
+            File.WriteAllLines(uniqueFileName, To);
+            File.AppendAllLines(uniqueFileName, Data);
+        }
+        
+        void SaveInternal(string username,bool isInbox)
+        {
+            
+            
+            string mailboxPath = Path.Combine(SMTPServer.SMTPServer.InboxPath, username);
+            if(!isInbox)
+            {
+                mailboxPath = Path.Combine(mailboxPath, "sent");
+            }
+
+            bool existaUser = false;
+            foreach(var user in File.ReadAllLines(SMTPServer.SMTPServer.Credentials))
+            {
+                string currentUser = user.Split(' ')[0];
+                if(username == currentUser)
+                {
+                    existaUser = true;
+                    break;
+                }
+            }
+            if (existaUser == false)
+                return;
+
+            DirectoryInfo directory = new DirectoryInfo(mailboxPath);
+            directory.Create();
+
+            FileInfo[] files = directory.GetFiles();
+            int lastUID = 0;
+            foreach (var currentFile in files)
+            {
+                string localUID = currentFile.Name.Split('.')[0];
+                if (Int32.Parse(localUID) > SMTPServer.SMTPServer.UID)
+                    SMTPServer.SMTPServer.UID = Int32.Parse(localUID);
+            }
+            SMTPServer.SMTPServer.UID++;
+
+            string filename = Path.Combine(mailboxPath, SMTPServer.SMTPServer.UID.ToString() + ".txt");
+            var file = File.Create(filename);
+            file.Close();
+            File.AppendAllLines(filename, Data.GetRange(1, Data.Count - 1));
+            SMTPServer.SMTPServer.UID++;
+        }
+    }
+}
